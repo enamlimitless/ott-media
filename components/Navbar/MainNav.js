4730fab50403d422c5289130e2aa6ef2babbbea3
enamulhaque7 <enamulhaquemb01@gmail.com>
2021-06-12 23:04:26 +0600

    updating nav items

--- components/Navbar/MainNav.js
+++ components/Navbar/MainNav.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import requests from '../../utils/requests';
 
 const MainNav = () => {
     return (
         <div>
-            
+            {
+                Object.entries(requests)
+            }
         </div>
     );
 };
--- utilis/requests.js
+++
@@ -1,8 +0,0 @@
-const API_KEY = process.env.API_KEY;
-
-export default {
-    fetchTreanding: {
-        title: 'Trending',
-        url: ``
-    }
-}
